caf20d0deb01bb282e263518a2d499322189765a
YoungHun Jeong <jyh7a@naver.com>
2021-11-14 21:41:55 +0900

    그랩마켓 서버 시작

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require("http"); // node 내장 모듈 불러옴
+var hostname = "127.0.0.1"; // localhost와 동일
+var port = 8080;
+
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "농구공",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다!");
+    }
+  }
+});
+
+server.listen(port, hostname);
+
+console.log("Server running at http://" + hostname + ":" + port);
